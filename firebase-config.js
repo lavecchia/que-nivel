2145114b6f52d7156a430e4ebb427b510310939e
lavecchia <lavecchia@gmail.com>
2025-10-15 23:51:04 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyD-Dc6jlhSy2wraMdZ46Mzq9Oj3wH5ZJcc",
+
+  authDomain: "semaforo-clase.firebaseapp.com",
+
+  projectId: "semaforo-clase",
+
+  storageBucket: "semaforo-clase.firebasestorage.app",
+
+  messagingSenderId: "874501450941",
+
+  appId: "1:874501450941:web:d0f0c1dbd8207958265bd4",
+
+  measurementId: "G-X3J5Y42QZG"
+
+};
